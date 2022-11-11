19bc3ec0fced41c4f4b04164ccbaf89f5ef4119b
maptile <28569363+maptile@users.noreply.github.com>
2022-11-11 10:12:36 +0800

    install docker compose plugin instead of standalone install

--- install.sh
+++ install.sh
@@ -49,11 +49,7 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-
-echo -e "${GREEN}Installing docker-compose${NC}"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo groupadd docker
 # sudo usermod -aG docker $USER
